e71b76c783cddc8590373befe2404564ec50e6dd
Grigoriy <grisha101202@yandex,ru>
2020-11-13 14:21:19 +0300

    ended 3

--- src/Three.java
+++ src/Three.java
@@ -1,2 +1,20 @@
+import java.util.*;
 public class Three {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt();
+        int d=0;
+        int flag=0;
+        for (int j = a; j <= b; j++) {
+            d=0;
+            for (int i = 1; i < j; i++) {
+                if (j % i == 0) d++;
+            }
+                if (d < 2) {
+                    System.out.print(" " + j);
+                    flag=1;
+                }
+        }
+        if(flag==0) System.out.print(flag);
+    }
 }
